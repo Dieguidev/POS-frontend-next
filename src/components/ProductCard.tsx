92d8f6226d4f7ec1dd905beebb2e6a6897104e68
Dieguidev <diegogaraycullas@gmail.com>
2025-02-12 22:00:24 -0500

    Agregar soporte para imágenes remotas en la configuración de Next.js y actualizar ProductCard para mostrar imágenes de productos

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Product } from '@/schemas/schemas'
 import { formatCurrency } from '@/utils/formatCurrency'
+import Image from 'next/image'
 import React from 'react'
 
 type ProductCardProps = {
@@ -11,7 +12,12 @@ export const ProductCard = ({ product }: ProductCardProps) => {
       className='rounded bg-white shadow relative p-5'
     >
       <div>
-
+        <Image
+          src={`${process.env.API_URL}/img/${product.image}`}
+          alt={`Imagen de Producto ${product.name}`}
+          width={400}
+          height={600}
+        />
         <div className="p-3 space-y-2">
           <h3 className="text-xl font-bold text-gray-600">{product.name}</h3>
           <p className="text-gray-500">Disponibles: {product.inventory}</p>
