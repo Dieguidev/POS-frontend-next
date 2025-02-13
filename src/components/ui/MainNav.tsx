a09e3a903b463e83a3eed421e32552ff780f0100
Dieguidev <diegogaraycullas@gmail.com>
2025-02-12 22:22:56 -0500

    Agregar enlaces de categorías en el componente MainNav

--- src/components/ui/MainNav.tsx
+++ src/components/ui/MainNav.tsx
@@ -1,11 +1,12 @@
 import { CategoriesSchema } from "@/schemas/schemas";
 import { Logo } from "./Logo"
+import Link from "next/link";
 
 async function getCategories() {
   const url = `${process.env.API_URL}/categories`;
   const req = await fetch(url, {
     method: 'GET',
-    cache:'force-cache',
+    cache: 'force-cache',
     next: {
       tags: [`categories`],
     }
@@ -26,13 +27,18 @@ export const MainNav = async () => {
 
   return (
     <header className="px-10 py-5 bg-gray-700 flex flex-col md:flex-row justify-between ">
-        <div className="flex justify-center">
-          <Logo />
-        </div>
+      <div className="flex justify-center">
+        <Logo />
+      </div>
 
-        <nav className="flex flex-col md:flex-row gap-2 items-center mt-5 md:mt-0">
-
-        </nav>
+      <nav className="flex flex-col md:flex-row gap-2 items-center mt-5 md:mt-0">
+        {
+          categories.map(category => (
+            <Link href={`${category.id}`} key={category.id} className="text-white hover:text-green-400 font-bold p-2"
+            >{category.name}</Link>
+          ))
+        }
+      </nav>
     </header>
   )
 }
