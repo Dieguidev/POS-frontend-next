05c8e90869f20e146e17d2a1c87031613b2c7bbe
Dieguidev <diegogaraycullas@gmail.com>
2025-02-12 20:52:19 -0500

    Actualizar StorePage para manejar parámetros asincrónicos y agregar configuración de EditorConfig

--- src/app/(store)/[categoryId]/page.tsx
+++ src/app/(store)/[categoryId]/page.tsx
@@ -1,5 +1,13 @@
-export default function StorePage() {
-    return (
-        <div>StorePage</div>
-    );
+
+type Params = Promise<{ categoryId: string }>
+export default async function StorePage({ params }: { params: Params }) {
+
+  const { categoryId } = await params;
+  console.log(categoryId);
+
+
+
+  return (
+    <div>StorePage</div>
+  );
 }
