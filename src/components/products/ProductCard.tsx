027de051971db7051230d5f13bb77df14d5ceac4
Dieguidev <diegogaraycullas@gmail.com>
2025-02-13 06:10:40 -0500

    Agregar botón para añadir productos al carrito y actualizar la gestión de estado

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from '@/schemas/schemas'
 import { formatCurrency } from '@/utils/formatCurrency'
 import Image from 'next/image'
 import React from 'react'
+import { AddProductButton } from './AddProductButton'
 
 type ProductCardProps = {
   product: Product
@@ -24,14 +25,7 @@ export const ProductCard = ({ product }: ProductCardProps) => {
           <p className="text-2xl font-extrabold  text-gray-900">{formatCurrency(product.price)}</p>
         </div>
       </div>
-      <button
-        type="button"
-        className="absolute top-5 -right-3"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 bg-indigo-600 rounded-full text-white">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </button>
+      <AddProductButton product={product}/>
     </div>
   )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,16 @@
+import { Product } from '@/schemas/schemas';
 import { create } from 'zustand';
 
 interface Store {
   total: number;
+
+  addToCart: (product: Product) => void;
 }
 
 export const useStore = create<Store>(() => ({
   total: 0,
+  addToCart: ()=>{
+    console.log('aqui');
+
+  }
 }));
