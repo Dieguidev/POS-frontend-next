1d064dc1e1dfb184a1feae397da513378cc63afe
Dieguidev <diegogaraycullas@gmail.com>
2025-02-15 17:13:19 -0500

    Agregar componente ProductsTable y lógica para mostrar productos en la página de administración

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,3 +1,4 @@
+import { ProductsTable } from "@/components/products/ProductsTable";
 import { Heading } from "@/components/ui/Heading";
 import { ProdcutsResponseSchema } from "@/schemas/schemas";
 
@@ -6,16 +7,23 @@ async function getProducts() {
   const url = `${process.env.API_URL}/products`
   const req = await fetch(url)
   const json = await req.json()
-  const products = ProdcutsResponseSchema.parse(json)
-
-  console.log(products);
-
+  const data = ProdcutsResponseSchema.parse(json)
+  return {
+    products: data.data,
+    total: data.total
+  }
 }
 
 export default async function ProductsPage() {
 
-  await getProducts()
+  const {products,total} = await getProducts()
   return (
-    <Heading>Administrar productos</Heading>
+    <>
+      <Heading>Administrar productos</Heading>
+
+      <ProductsTable
+      products={products}
+      />
+    </>
   );
 }
