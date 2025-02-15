0d3461d4fa787a6573389ec2604327fc2eff3a20
Dieguidev <diegogaraycullas@gmail.com>
2025-02-15 17:40:23 -0500

    Agregar validación de página y paginación en la obtención de productos en la página de administración

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,28 +1,41 @@
 import { ProductsTable } from "@/components/products/ProductsTable";
 import { Heading } from "@/components/ui/Heading";
 import { ProdcutsResponseSchema } from "@/schemas/schemas";
+import { isValidPage } from "@/utils/isValidPage";
+import { redirect } from "next/navigation";
 
 
-async function getProducts() {
-  const url = `${process.env.API_URL}/products`
+async function getProducts(limit: number, page: number) {
+  const url = `${process.env.API_URL}/products?limit=${limit}&page=${page}`
   const req = await fetch(url)
   const json = await req.json()
   const data = ProdcutsResponseSchema.parse(json)
+
   return {
     products: data.data,
     total: data.total
   }
 }
 
-export default async function ProductsPage() {
+type SearchParams = Promise<{ page: string }>
 
-  const {products,total} = await getProducts()
+export default async function ProductsPage({ searchParams }: { searchParams: SearchParams }) {
+
+  const { page } = await searchParams
+  if (!isValidPage(+page)) {
+    redirect('/admin/products?page=1')
+  }
+
+  const productsPorPage = 10
+  // const skip = (+page) * productsPorPage
+
+  const { products, total } = await getProducts(productsPorPage, +page)
   return (
     <>
       <Heading>Administrar productos</Heading>
 
       <ProductsTable
-      products={products}
+        products={products}
       />
     </>
   );
