7bd011d0d4236fadea4dcdb6dbb3f59d5fcd3b22
Dieguidev <diegogaraycullas@gmail.com>
2025-02-15 09:28:57 -0500

    Agregar funcionalidad de selección de fecha en el filtro de transacciones

--- src/components/transactions/TransactionFilter.tsx
+++ src/components/transactions/TransactionFilter.tsx
@@ -1,12 +1,26 @@
-'use cliente'
+'use client'
 
+import { useState } from "react"
 import Calendar from "react-calendar"
+import 'react-calendar/dist/Calendar.css'
+
+type ValuePiece = Date | null
+type Value = ValuePiece | [ValuePiece, ValuePiece]
 
 export const TransactionFilter = () => {
+
+  const [date, setDate] = useState<Value>(new Date())
+
+  console.log(date);
+
+
   return (
-    <div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
       <div>
-        <Calendar />
+        <Calendar
+        value={date}
+        onChange={setDate}
+        />
       </div>
       <div>
 
