76e788cba297629c46901d6643c80664943c86ab
Dieguidev <diegogaraycullas@gmail.com>
2025-02-15 21:40:51 -0500

    Agregar lógica para agregar productos con validación y manejo de errores en el formulario

---
+++ src/actions/add-product action.ts
@@ -0,0 +1,49 @@
+'use server';
+
+import { ErrorResponseSchema, ProductFormSchema, SuccessResponseSchema } from "@/schemas/schemas";
+
+type ActionStatetype = {
+  errors: string[];
+  success: string;
+}
+
+export async function addProduct(prevState: ActionStatetype, formData: FormData) {
+  const product = ProductFormSchema.safeParse({
+    name: formData.get('name'),
+    price: Number(formData.get('price')),
+    inventory: Number(formData.get('inventory')),
+    categoryId: Number(formData.get('categoryId')),
+  });
+  if(!product.success) {
+    return {
+      errors: product.error.issues.map((issue) => issue.message),
+      success: '',
+    };
+  }
+
+  const url = `${process.env.API_URL}/products`;
+  const req = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(product.data),
+  });
+  const json = await req.json();
+
+  if (!req.ok) {
+    const errors = ErrorResponseSchema.parse(json);
+    return {
+      errors: errors.message.map((issue) => issue),
+      success: '',
+    };
+  }
+
+  const success = SuccessResponseSchema.parse(json);
+
+
+  return {
+    errors: [],
+    success: success.message,
+  };
+}
--- src/components/products/AddProductForm.tsx
+++ src/components/products/AddProductForm.tsx
@@ -1,12 +1,40 @@
 'use client'
 
-export const AddProductForm = ({children}: {children: React.ReactNode}) => {
+import { addProduct } from "@/actions/add-product action"
+import { useRouter } from "next/navigation"
+
+import { useActionState, useEffect } from "react"
+import { toast } from "react-toastify"
+
+export const AddProductForm = ({ children }: { children: React.ReactNode }) => {
+
+  const router = useRouter()
+
+  const [state, dispatch] = useActionState(addProduct, {
+    errors: [],
+    success: ''
+  })
+
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach((error: string) => toast.error(error))
+    }
+    if (state.success) {
+      toast.success(state.success)
+      router.push('/admin/products')
+    }
+  }, [state, router])
+
+
   return (
-    <form className="space-y-5">
+    <form
+      className="space-y-5"
+      action={dispatch}
+    >
       {children}
       <input type="submit"
-      className='rounded bg-green-400 font-bold py-2 w-full cursor-pointer'
-      value='Agregar producto'
+        className='rounded bg-green-400 font-bold py-2 w-full cursor-pointer'
+        value='Agregar producto'
       />
     </form>
   )
